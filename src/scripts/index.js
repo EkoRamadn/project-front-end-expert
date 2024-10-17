5f09517f926932c42cede28bf70439f548b1bef7
EkoRamadn <ekoramdani7777@gmail.com>
2024-10-17 21:07:14 +0700

    prototy day 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,17 +3,11 @@ import '../styles/main.scss';
 
 console.log('Hello Coders! :)');
 
-const sliderContainer = document.querySelector('.slider-container');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+const menuBtn = document.querySelector(".menu-btn")
 
-let index = 0;
-
-function updateSliderPosition() {
-  const cardWidth = document.querySelector('.card-container').offsetWidth;
-  sliderContainer.style.transform = `translateX(-${index * (cardWidth + 32)}px)`; // 32px adalah gap antar kartu
-}
-
+menuBtn.addEventListener('click', ()=>{
+  menuBtn.classList.toggle('active')
+})
 
 
 
