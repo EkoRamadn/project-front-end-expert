c9d16c27d5ebf29a2112d9f428d170db33fb0e9a
EkoRamadn <ekoramdani7777@gmail.com>
2024-10-17 08:56:56 +0700

    prototy 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,18 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
 
 console.log('Hello Coders! :)');
+
+const sliderContainer = document.querySelector('.slider-container');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let index = 0;
+
+function updateSliderPosition() {
+  const cardWidth = document.querySelector('.card-container').offsetWidth;
+  sliderContainer.style.transform = `translateX(-${index * (cardWidth + 32)}px)`; // 32px adalah gap antar kartu
+}
+
+
+
+
